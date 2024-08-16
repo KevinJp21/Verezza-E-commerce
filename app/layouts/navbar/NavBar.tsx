5874729d450b3c7b54039f1826211248e8e4836a
KevinJp21 <kevinjp821@gmail.com>
2024-08-16 17:47:11 -0500

    finish navbar

--- app/layouts/navbar/NavBar.tsx
+++ app/layouts/navbar/NavBar.tsx
@@ -1,9 +1,54 @@
 import './NavBar.css'
+import logoOlgaBlack from '~/assets/logos/Logo Olga black.webp'
+import logoOlgaWhite from '~/assets/logos/Logo Olga white.webp'
 
+import { heartIcon, userIcon, cartIcon } from '~/assets/icons/icons';
 export default function NavBar() {
-  return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
-  );
+    return (
+        <header className='navbar'>
+            <nav className='NavBarContainer'>
+                <div className='NavBar1'>
+                    <a href="/">
+                        <img src={logoOlgaWhite} alt='Logo de la marca Olga Lucía Cortes' width={290} height={20} decoding='async' loading='lazy' />
+                    </a>
+                    <div className='NavBar1-1'>
+                        <a href="" className="accoutHeader">
+                            {userIcon()}
+                        </a>
+                        <a href="/" className="whishlistHeader">
+                            {heartIcon()}
+                        </a>
+                        <a href="/" className="whishlistHeader">
+                            {cartIcon()}
+                        </a>
+                    </div>
+                </div>
+                <div className='NavBar2'>
+                    <ul className='NavbarListWrapper'>
+                        <li>
+                            <a href="">BLUSAS</a>
+                        </li>
+                        <li>
+                            <a href="">PANTALONES</a>
+                        </li>
+                        <li>
+                            <a href="">VESTIDOS</a>
+                        </li>
+                        <li>
+                            <a href="">CONJUNTOS</a>
+                        </li>
+                        <li>
+                            <a href="">COMPLEMENTOS</a>
+                        </li>
+                        <li>
+                            <a href="">ATÉLIER</a>
+                        </li>
+                        <li>
+                            <a href="" className='strongLink'>NUESTRA MARCA</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,18 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { LinksFunction } from "@remix-run/node";
+import './styles.css'
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com'},
+    { rel: 'preload', as: 'style', href: 'https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic&amp;display=swap&amp;ver=1691393934' },
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic&amp;display=swap&amp;ver=1691393934' }
+
+  ]
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
