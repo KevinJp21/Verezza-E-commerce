cf8cc3767bb82c5675e8d29c429c54aaaf8a6ee4
KevinJp21 <kevinjp821@gmail.com>
2024-08-17 17:35:08 -0500

    add hover to wear images

--- app/sections/Home/SectionWear/WearHome.tsx
+++ app/sections/Home/SectionWear/WearHome.tsx
@@ -1,4 +1,5 @@
 import './WearHome.css'
+
 import imgWearComplements from '~/assets/images/SectionWear/complementos.webp'
 import imgWearComplements2 from '~/assets/images/SectionWear/Complementos2.webp'
 import imgWearBlouse from '~/assets/images/SectionWear/wear-blouse.webp'
@@ -10,8 +11,9 @@ export default function WearHome(){
     return(
         <section className='WearHomeContainer'>
             <div className='WearHomeWrapper'>
-                <div className='WearImg img1'>
-                    <img src={imgWearComplements} alt="Imagen complementos olga lucia cortes" loading='lazy' decoding='async' />
+                <div className='WearImg img1 group relative'>
+                    <img className='group-hover:hidden' src={imgWearComplements} alt="Imagen complementos olga lucia cortes" loading='lazy'/>
+                    <img className='hidden group-hover:block' src={imgWearComplements2} alt="Imagen complementos olga lucia cortes" loading='lazy' />
                     <div className='WearImgText'>
                         <h3>Complementos</h3>
                         <p>El toque final perfecto: un cinturón que redefine tu estilo con elegancia.</p>
@@ -19,8 +21,9 @@ export default function WearHome(){
                     </div>
                 </div>
 
-                <div className='WearImg img2'>
-                    <img src={imgWearBlouse} alt="Imagen blusa olga lucia cortes" loading='lazy' decoding='async' />
+                <div className='WearImg img2 group relative'>
+                    <img className='group-hover:hidden' src={imgWearBlouse} alt="Imagen blusa olga lucia cortes" loading='lazy'/>
+                    <img className='hidden group-hover:block' src={imgWearBlouse2} alt="Imagen blusa olga lucia cortes" loading='lazy' />
                     <div className='WearImgText'>
                         <h3>Blusas</h3>
                         <p>Blusas que combinan comodidad y sofisticación para cualquier ocasión.</p>
