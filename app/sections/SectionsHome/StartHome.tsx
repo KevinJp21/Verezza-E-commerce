d9410355acf45947933d2f52d86f340ff3600bf5
KevinJp21 <kevinjp821@gmail.com>
2024-08-17 14:08:32 -0500

    finish section startHome

--- app/sections/SectionsHome/StartHome.tsx
+++ app/sections/SectionsHome/StartHome.tsx
@@ -2,17 +2,21 @@ import './StartHome.css'
 import imgStartHome1 from '../../assets/images/StartSectionHome/banner01.webp'
 import imgStartHome2 from '../../assets/images/StartSectionHome/banner04.webp'
 import imgStartHome3 from '../../assets/images/StartSectionHome/banner02.webp'
+import imgStartHome4 from '../../assets/images/StartSectionHome/banner03.webp'
 
 export default function StartHome() {
   return( 
   <section className="StartHomeContainer">
-    <picture className="StartHomeImg">
+    <div className="StartHomeImg">
         <img className='img1' src={imgStartHome1} alt="Olga Lucía Cortes" />
         <img className='img2' src={imgStartHome2} alt="Olga Lucía Cortes" />
         <img className='img3' src={imgStartHome3} alt="Olga Lucía Cortes" />
-    </picture>
+        <img className='img4' src={imgStartHome4} alt="Olga Lucía Cortes" />
+    </div>
     <div className="StartHomeWrapper">
-      
+      <h1>Descubre la moda que te hará sentir única</h1>
+      <h2>¡Bienvenida a Olga Lucía Cortés!</h2>
+      <a href='#'><span>Explora ahora</span></a>
     </div>
   </section>
   )
