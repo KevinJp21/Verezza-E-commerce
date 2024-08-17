16650bbfca746b81019611c76b07551226bc0c23
KevinJp21 <kevinjp821@gmail.com>
2024-08-17 13:28:08 -0500

    create section startHome

--- app/sections/SectionsHome/StartHome.tsx
+++ app/sections/SectionsHome/StartHome.tsx
@@ -1,9 +1,19 @@
 import './StartHome.css'
+import imgStartHome1 from '../../assets/images/StartSectionHome/banner01.webp'
+import imgStartHome2 from '../../assets/images/StartSectionHome/banner04.webp'
+import imgStartHome3 from '../../assets/images/StartSectionHome/banner02.webp'
 
 export default function StartHome() {
-  return <div className="StartHome">
-    <div className="ContainerStartHome">
+  return( 
+  <section className="StartHomeContainer">
+    <picture className="StartHomeImg">
+        <img className='img1' src={imgStartHome1} alt="Olga Lucía Cortes" />
+        <img className='img2' src={imgStartHome2} alt="Olga Lucía Cortes" />
+        <img className='img3' src={imgStartHome3} alt="Olga Lucía Cortes" />
+    </picture>
+    <div className="StartHomeWrapper">
       
     </div>
-  </div>;
+  </section>
+  )
 }
