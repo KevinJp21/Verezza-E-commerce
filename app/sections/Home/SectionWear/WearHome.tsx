e3bc5b27ea63caa5cfaac952a44e6df5320e1ed5
KevinJp21 <kevinjp821@gmail.com>
2024-08-17 18:08:34 -0500

    finish section wear

--- app/sections/Home/SectionWear/WearHome.tsx
+++ app/sections/Home/SectionWear/WearHome.tsx
@@ -11,9 +11,9 @@ export default function WearHome(){
     return(
         <section className='WearHomeContainer'>
             <div className='WearHomeWrapper'>
-                <div className='WearImg img1 group relative'>
-                    <img className='group-hover:hidden' src={imgWearComplements} alt="Imagen complementos olga lucia cortes" loading='lazy'/>
-                    <img className='hidden group-hover:block' src={imgWearComplements2} alt="Imagen complementos olga lucia cortes" loading='lazy' />
+                <div className='WearImg img1'>
+                    <img className='img1-1' src={imgWearComplements} alt="Imagen complementos olga lucia cortes" loading='lazy' decoding='async' width={780} height={1200} />
+                    <img className='img1-2' src={imgWearComplements2} alt="Imagen complementos olga lucia cortes" loading='lazy' decoding='async' width={780} height={1200} />
                     <div className='WearImgText'>
                         <h3>Complementos</h3>
                         <p>El toque final perfecto: un cinturón que redefine tu estilo con elegancia.</p>
@@ -21,9 +21,9 @@ export default function WearHome(){
                     </div>
                 </div>
 
-                <div className='WearImg img2 group relative'>
-                    <img className='group-hover:hidden' src={imgWearBlouse} alt="Imagen blusa olga lucia cortes" loading='lazy'/>
-                    <img className='hidden group-hover:block' src={imgWearBlouse2} alt="Imagen blusa olga lucia cortes" loading='lazy' />
+                <div className='WearImg img2'>
+                    <img className='img1-1' src={imgWearBlouse} alt="Imagen blusa olga lucia cortes" loading='lazy' decoding='async' width={780} height={1200} />
+                    <img className='img1-2' src={imgWearBlouse2} alt="Imagen blusa olga lucia cortes" loading='lazy' decoding='async' width={780} height={1200} />
                     <div className='WearImgText'>
                         <h3>Blusas</h3>
                         <p>Blusas que combinan comodidad y sofisticación para cualquier ocasión.</p>
