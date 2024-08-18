e0f1c2aad43a2106823634e0d24e1195940b09fd
KevinJp21 <kevinjp821@gmail.com>
2024-08-18 15:29:01 -0500

    improve performance on mobile devices

--- app/sections/Home/SectionsHome/StartHome.tsx
+++ app/sections/Home/SectionsHome/StartHome.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import './StartHome.css'
 import imgStartHome1 from '~/assets/images/StartSectionHome/banner01.webp'
 import imgStartHome2 from '~/assets/images/StartSectionHome/banner04.webp'
@@ -5,13 +6,25 @@ import imgStartHome3 from '~/assets/images/StartSectionHome/banner02.webp'
 import imgStartHome4 from '~/assets/images/StartSectionHome/banner03.webp'
 
 export default function StartHome() {
+  
+  const [isMobile, setIsMobile] = useState(false)
+
+  const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
+  }
+
+  useEffect(() => {
+      window.addEventListener('resize', handleResize)
+      return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
   return( 
   <section className="StartHomeContainer">
     <div className="StartHomeImg">
-        <img className='img1' src={imgStartHome1} alt="Olga Lucía Cortes" loading="eager" />
-        <img className='img2' src={imgStartHome2} alt="Olga Lucía Cortes" loading="eager" />
-        <img className='img3' src={imgStartHome3} alt="Olga Lucía Cortes" loading="eager" />
-        <img className='img4' src={imgStartHome4} alt="Olga Lucía Cortes" loading="eager" />
+        <img className='img1' src={imgStartHome1} alt="Olga Lucía Cortes" loading={isMobile ? 'lazy' : 'eager'} decoding={isMobile ? 'async' : 'auto'}/>
+        <img className='img2' src={imgStartHome2} alt="Olga Lucía Cortes" loading={isMobile ? 'lazy' : 'eager'} decoding={isMobile ? 'async' : 'auto'}/>
+        <img className='img3' src={imgStartHome3} alt="Olga Lucía Cortes" loading={isMobile ? 'lazy' : 'eager'} decoding={isMobile ? 'async' : 'auto'}/>
+        <img className='img4' src={imgStartHome4} alt="Olga Lucía Cortes" loading={isMobile ? 'lazy' : 'eager'} decoding={isMobile ? 'async' : 'auto'}/>
     </div>
     <div className="StartHomeWrapper">
       <h1>Descubre la moda que te hará sentir única</h1>
