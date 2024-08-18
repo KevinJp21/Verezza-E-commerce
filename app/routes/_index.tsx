c6387fc2771ffa4f7760e6e650835798fc83cb74
KevinJp21 <kevinjp821@gmail.com>
2024-08-18 17:14:10 -0500

    updateStartHome.css

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,23 +4,23 @@ import Home from "~/pages/home/home";
 
 export const meta: MetaFunction = () => {
   const currentDate = new Date().toISOString();
-  const baseUrl = "https://olga-lucia-cortes.vercel.app/";
+ 
   return [
     { title: "Olga Lucía Cortes – Moda exclusiva para mujeres reales" },
     { name: "description", content: "Descubre la moda exclusiva de Olga Lucía Cortés. Encuentra blusas, pantalones, vestidos y más, diseñados para mujeres reales. Compra ahora y complementa tu look con nuestras colecciones únicas." },
     { name: "keywords", content: "moda, ropa de mujer, blusas, pantalones, vestidos, accesorios, moda colombiana, talla s, talla m, talla l, talla xl, talla xxl, complementos" },
     { name: "og:title", content: "Olga Lucía Cortes - Moda exclusiva para mujeres reales" },
     { name: "og:description", content: "Descubre nuestra colección de moda diseñada para resaltar la belleza de todas las mujeres. Prendas únicas y elegantes para cada ocasión." },
-    { name: "og:image", content: `${baseUrl}/HomeScreen.webp` },
+    { name: "og:image", content: `https://olga-lucia-cortes.vercel.app/seo/HomeScreen.webp` },
     { name: "og:image:alt", content: "Página de inicio de Olga Lucía Cortés" },
     { name: "og:image:width", content: "1200" },
     { name: "og:image:height", content: "630" },
     { name: "og:type", content: "website" },
-    { name: "og:url", content: `${baseUrl}` },
+    { name: "og:url", content: "https://olga-lucia-cortes.vercel.app" },
     { name: "twitter:card", content: "summary_large_image" },
     { name: "twitter:title", content: "Olga Lucía Cortes – Moda exclusiva para mujeres reales" },
     { name: "twitter:description", content: "Descubre la moda exclusiva de Olga Lucía Cortés. Encuentra blusas, pantalones, vestidos y más, diseñados para mujeres reales. Compra ahora y complementa tu look con nuestras colecciones únicas." },
-    { name: "twitter:image", content: `${baseUrl}/seo/HomeScreen.webp` },
+    { name: "twitter:image", content: `https://olga-lucia-cortes.vercel.app/seo/HomeScreen.webp` },
     { name: "og:updated_time", content: currentDate },
   ];
 };
