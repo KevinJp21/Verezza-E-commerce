c4c9c5071679d11ed01913ed23bab4cd3c33a05c
KevinJp21 <kevinjp821@gmail.com>
2024-10-01 11:29:58 -0500

    update font

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import './styles.css'
 import { ProductProvider } from './hooks/ProductContext';
 import i18nServer from "./modules/i18n.server";
 import { useChangeLanguage } from "remix-i18next/react";
+import NimbusSans from '~/assets/fonts/fonts.css?url';
 
 export const handle = { i18n: ["translation"] }
 
@@ -27,7 +28,8 @@ export const links: LinksFunction = () => {
   return [
     { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
     { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: 'use-credentials' },
-    { rel:"stylesheet", href:"https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"}
+    { rel:"stylesheet", href:"https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"},
+    { rel: 'stylesheet', href: NimbusSans }
   ]
 }
 
