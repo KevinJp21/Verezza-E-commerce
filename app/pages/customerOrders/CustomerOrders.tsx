0ef7677384bd5c59738031a7af9b97015a829b71
KevinJp21 <kevinjp821@gmail.com>
2024-10-16 19:01:50 -0500

    Add styling to orderItem when it has more than one product

--- app/pages/customerOrders/CustomerOrders.tsx
+++ app/pages/customerOrders/CustomerOrders.tsx
@@ -67,12 +67,21 @@ export default function CustomerOrders({ data }: { data: CustomerData }) {
                                 {order.canceledAt !== null ? cancelIcon() : order.fulfillmentStatus === 'FULFILLED' ? truckIcon() : pendingIcon()}
                                 <span>{order.canceledAt !== null ? 'Cancelado' : order.fulfillmentStatus === 'FULFILLED' ? 'En camino' : 'Pendiente'} - {new Date(order.processedAt).toLocaleDateString('es-ES', { month: 'short', day: 'numeric' })}</span>
                             </div>
-                            <picture className='orderItemImage'>
-                                <img src={order.lineItems.edges[0].node.variant.product.images.nodes[0].url} alt={order.fulfillmentStatus} height={300} />
-                            </picture>
-
+                            <div className="orderImages">
+                                {order.lineItems.edges.map(({ node: item }) => (
+                                    item.variant.product.images.nodes.map((image, index) => (
+                                        <picture key={`${item.variant.product.title}-${index}`} className='orderItemImage'>
+                                            <img
+                                                src={image.url}
+                                                alt={`${item.variant.product.title} - Imagen ${index + 1}`}
+                                                height={100}
+                                            />
+                                        </picture>
+                                    ))
+                                ))}
+                            </div>
                             <div className="orderDetails">
-                                <div className="orderItemCount"><span>Cantidad: {order.lineItems.edges.reduce((total, item) => total + item.node.quantity, 0)}</span></div>
+                                <div className="orderItemCount"><span>{order.lineItems.edges.reduce((total, item) => total + item.node.quantity, 0)} Articulos</span></div>
                                 <div className="orderNumber"><span>Pedido: {order.name}</span></div>
                                 <div className="orderPrice"><span>{parseFloat(order.totalPrice.amount).toLocaleString('es-ES', { style: 'currency', currency: order.totalPrice.currencyCode, minimumFractionDigits: 0 })}</span></div>
                             </div>
